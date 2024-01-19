c9c3f2841e25bef90c4da692e03f80ee7802f15d
Nagendra <venkatanagendra0004@gmail.com>
2024-01-19 12:47:09 +0530

    first commit

---
+++ SampleForGit/src/Git/SampleClass.java
@@ -0,0 +1,13 @@
+package Git;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="Nagendra";
+		String b="Babu";
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+}
